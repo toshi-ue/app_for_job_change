9bb3c867490ef7f6e05e864d691f6f9571e3359c
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-01 20:32:40 +0900

    WIP createアクションのテストがパスしない

--- spec/factories/foodstuffs.rb
+++ spec/factories/foodstuffs.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :foodstuff do
-    
+    association :cuisine
+    association :rawmaterial
+    sequence(:quantity) {|n| "#{n}" }
+    # sequence(:row_order) {|n| "#{n}" }
   end
 end
--- spec/requests/managers/foodstuffs_request_spec.rb
+++ spec/requests/managers/foodstuffs_request_spec.rb
@@ -1,26 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "Managers::Foodstuffs", type: :request do
+  before do
+    manager = create(:manager)
+    sign_in manager
+    get root_path
+    @cuisine = create(:cuisine)
+  end
 
   describe "GET /index" do
-    it "returns http success" do
-      get "/managers/foodstuffs/index"
-      expect(response).to have_http_status(:success)
-    end
   end
 
   describe "GET /new" do
-    it "returns http success" do
-      get "/managers/foodstuffs/new"
-      expect(response).to have_http_status(:success)
+    it "リクエストが成功すること" do
+      get new_managers_foodstuff_path(cuisine_id: @cuisine.id)
+      expect(response.status).to eq 200
+    end
+
+    it "newテンプレートで表示されること" do
+      get new_managers_foodstuff_path(cuisine_id: @cuisine.id)
+      expect(response.body).to include "食材を検索"
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/managers/foodstuffs/edit"
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+    it "リクエストが成功すること" do
+      post managers_foodstuffs_path, params: {
+        foodstuff: FactoryBot.attributes_for(:foodstuff, cuisine_id: @cuisine.id)
+      }
+      expect(response.status).to eq 302
+    end
+
+    it "新規レコードが作成されること" do
+      expect do
+        post managers_foodstuffs_path, params: { foodstuff: attributes_for(:foodstuff, cuisine_id: @cuisine.id)}
+      end.to change(Foodstuff, :count).by(1)
     end
   end
-
 end
