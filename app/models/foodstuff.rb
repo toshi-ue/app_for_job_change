b51024bf59e3c5c4a264313692206c0de4bb5565
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-03 20:39:59 +0900

    update: 不要なコメントを削除

--- app/models/foodstuff.rb
+++ app/models/foodstuff.rb
@@ -1,6 +1,5 @@
 class Foodstuff < ApplicationRecord
-  before_validation :convert_specific_format
-  # before_save :convert_specific_format
+  before_validation :convert_specific_format # 空白を削除, 全角があれば半角に
   belongs_to :cuisine
   belongs_to :rawmaterial
 
@@ -10,33 +9,8 @@ class Foodstuff < ApplicationRecord
   # FIXME: validationなしでもrawmaterial_id は validationがかかる
   #         なぜ?
   # validates :rawmaterial_id, presence: { message: "を追加してください"}
-  # ranked-model
-  include RankedModel
-  ranks :row_order, with_same: :cuisine_id
-
-  # quantityのvalidation
-  #  validationがokなら値を半角に変換して保存したい
-  #   もとはstring
-  #   変換する必要あり
-  #   整数(大文字、小文字)ならok
-  #     ok
-  #       1
-  #       １２
-  #       1３
-  #   分数(大文字、小文字)ならok
-  #     ok
-  #       1/1
-  #       1 / 3
-  #       １／１
-  #       1　／ 　　１
-  #   それ以外はng
-  #   okであれば大文字の場合は小文字に変換
-
-  # validates :cannot_save_except_specific_format
 
   def convert_specific_format
-    # 空白を削除, 全角があれば半角に
-    # self.quantity = quantity.gsub(/\ \　/, "").tr("／", "/").strip.tr('０-９', '0-9')
     self.quantity = quantity.gsub(/ |　/, "").tr("／", "/").strip.tr('０-９', '0-9')
   end
 end
