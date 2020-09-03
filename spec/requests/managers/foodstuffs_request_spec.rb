6337e7913dd3012d2d2820eadf9d83005e723958
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-03 21:48:37 +0900

    idtranslation missingエラーが発生する

--- spec/requests/managers/foodstuffs_request_spec.rb
+++ spec/requests/managers/foodstuffs_request_spec.rb
@@ -8,10 +8,11 @@ RSpec.describe "Managers::Foodstuffs", type: :request do
     @cuisine = create(:cuisine)
   end
 
+  # TODO: 最終的には必要ない
   describe "GET /index" do
   end
 
-  describe "GET /new" do
+  describe "GET #new" do
     it "リクエストが成功すること" do
       get new_managers_foodstuff_path(cuisine_id: @cuisine.id)
       expect(response.status).to eq 200
@@ -24,17 +25,109 @@ RSpec.describe "Managers::Foodstuffs", type: :request do
   end
 
   describe "POST #create" do
+    before do
+      @rawmaterial = create(:rawmaterial)
+    end
+
     it "リクエストが成功すること" do
       post managers_foodstuffs_path, params: {
-        foodstuff: FactoryBot.attributes_for(:foodstuff, cuisine_id: @cuisine.id)
+        foodstuff: {
+          cuisine_id: @cuisine.id,
+          rawmaterial_id: @rawmaterial.id,
+          quantity: 1
+        }
       }
       expect(response.status).to eq 302
     end
 
     it "新規レコードが作成されること" do
       expect do
-        post managers_foodstuffs_path, params: { foodstuff: attributes_for(:foodstuff, cuisine_id: @cuisine.id)}
+        post managers_foodstuffs_path, params: {
+          foodstuff: {
+            cuisine_id: @cuisine.id,
+            rawmaterial_id: @rawmaterial.id,
+            quantity: 1
+          }
+        }
       end.to change(Foodstuff, :count).by(1)
+    end
+
+    it "cuisine/showページへリダイレクトすること" do
+      post managers_foodstuffs_path, params: {
+        foodstuff: {
+          cuisine_id: @cuisine.id,
+          rawmaterial_id: @rawmaterial.id,
+          quantity: 1
+        }
+      }
+      follow_redirect!
+      expect(response.body).to include "#{@cuisine.name}"
+    end
+  end
+
+  describe "GET #edit" do
+    before do
+      @foodstuff = create(:foodstuff)
+    end
+
+    it "リクエストが成功すること" do
+      get edit_managers_foodstuff_path @foodstuff
+      expect(response.status).to eq 200
+    end
+
+    # TODO: JS:trueを使用したテストが必要
+    # it "editテンプレートで表示されること" do
+    #   get edit_managers_foodstuff_path @foodstuff
+    #   expect(response.body).to include "#{@foodstuff.rawmaterial.name}"
+    # end
+  end
+
+  describe "PATCH #update" do
+    before do
+      @foodstuff = create(:foodstuff, quantity: "10")
+    end
+
+    it "既存レコードが更新されること" do
+      put managers_foodstuff_path @foodstuff, params: {
+        foodstuff:{
+          cuisine_id: @foodstuff.cuisine_id,
+          rawmaterial_id: @foodstuff.rawmaterial_id,
+          quantity: "1/2"
+        }
+      }
+      expect(Foodstuff.find_by(id: @foodstuff.id).quantity). to eq "1/2"
+    end
+    it "cuisine/showページヘリダイレクトすること" do
+      put managers_foodstuff_path @foodstuff, params: {
+        foodstuff:{
+          cuisine_id: @foodstuff.cuisine_id,
+          rawmaterial_id: @foodstuff.rawmaterial_id,
+          quantity: "1/2"
+        }
+      }
+      expect(response).to redirect_to(managers_cuisine_path( @foodstuff.cuisine_id))
+    end
+  end
+
+  describe "DELETE #destroy" do
+    before do
+      @foodstuff = create(:foodstuff, quantity: "10")
+    end
+
+    it "リクエストが成功すること" do
+      delete managers_foodstuff_path @foodstuff
+      expect(response.status).to eq 302
+    end
+
+    it "既存レコードが削除されること" do
+      expect do
+        delete managers_foodstuff_path @foodstuff
+      end.to change(Foodstuff, :count).by(-1)
+    end
+
+    it "cuisine/showページヘリダイレクトすること" do
+      delete managers_foodstuff_path @foodstuff
+      expect(response).to redirect_to(managers_cuisine_path( @foodstuff.cuisine_id))
     end
   end
 end
