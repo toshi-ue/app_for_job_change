7ee48c73e7a417d95c5984b4bb62454a70b223fa
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-04 15:40:14 +0900

    add: foodstuffのテストを追加

--- app/models/foodstuff.rb
+++ app/models/foodstuff.rb
@@ -2,14 +2,7 @@ class Foodstuff < ApplicationRecord
   before_validation :convert_specific_format # 空白を削除, 全角があれば半角に
   belongs_to :cuisine
   belongs_to :rawmaterial
-  validates :cuisine_id,
-    uniqueness: {
-      scope: :rawmaterial_id,
-      message: ->(object, data){
-        I18n.t('activerecord.errors.models.cuisine.cannot_duplicate_rawmaterial')
-        # "1つの料理に同じ材料を登録できません"
-      }
-    }
+  validate :uniqueness_rawmaterial_id
   validates :quantity, presence: true, format: { with: %r{\A[1-9１-９]*[/／]*[0-9０-９]*\z}, message: "は分数もしくは数字で入力してください(例: 1/2, 120など)" }
   # ranked-model
   include RankedModel
@@ -18,4 +11,10 @@ class Foodstuff < ApplicationRecord
   def convert_specific_format
     self.quantity = quantity.gsub(/ |　/, "").tr("／", "/").strip.tr('０-９', '0-9')
   end
+
+  private
+    def uniqueness_rawmaterial_id
+      foodstuff = Foodstuff.find_by(cuisine_id: self.cuisine_id, rawmaterial_id: self.rawmaterial_id)
+      errors.add(:base, "1つの料理に同じ材料を登録できません") if foodstuff && foodstuff.id != self.id
+    end
 end
