4a06c801e73882a8c732fabda88e0065b51e0593
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-22 19:04:55 +0900

    WIP: ログイン後にfavoriteボタンを押しても反応しない

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,20 +1,20 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
-  # TODO: セレクトボックスでソート機能を実装したい
+  # TODO: セレクトボックスでソート機能を実装するか検討
   def index
     @favorites = current_user.favorites
   end
 
-  # TODO: あとでAjaxにする
+  # FIXME: あとでAjaxにする
   def create
+    @cuisine = Cuisine.find_by(id: params[:cuisine_id])
     @favorite = current_user.favorites.create(cuisine_id: params[:cuisine_id])
-    redirect_to cuisine_path(params[:cuisine_id])
   end
 
   def destroy
+    @cuisine = Cuisine.find_by(id: params[:cuisine_id])
     @favorite = current_user.favorites.find_by(cuisine_id: params[:cuisine_id])
     @favorite.destroy
-    redirect_to cuisine_path(params[:cuisine_id])
   end
 end
