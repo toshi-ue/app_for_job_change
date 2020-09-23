dbab83d0295544236071a862b55c527311476c9c
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-23 11:33:01 +0900

    binding.pryを追加、自作メソッドにコメントアウトを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :timeoutable, :trackable
 
   has_many :favorites, dependent: :destroy
+  # Ajaxで使用している自作メソッド
   def already_favorite?(cuisine)
     self.favorites.exists?(cuisine_id: cuisine.id)
   end
