1725ad6aa200f4642dba34299d2b33277e7e20f3
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-26 20:12:40 +0900

    add: gコマンドによるtodaysmenu関連のファイルを追加、ルーティングを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,13 @@ class User < ApplicationRecord
   def already_favorite?(cuisine)
     self.favorites.exists?(cuisine_id: cuisine.id)
   end
+
+  def already_menu_in_today?(user_id, cuisine_id)
+    todaysmenu = Todaysmenu.where(user_id: user_id, cuisine_id: cuisine_id, created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    if todaysmenu.blank?
+      false
+    else
+      true
+    end
+  end
 end
