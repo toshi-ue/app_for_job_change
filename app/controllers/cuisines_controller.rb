ab97938e08c5828ca168eddb2b807084c7cf25f3
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-26 20:14:16 +0900

    WIP ajaxでdestroyアクションが動作しない

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -5,4 +5,26 @@ class CuisinesController < ApplicationController
     # @genres
     @procedures = Procedure.where(cuisine_id: @cuisine.id)
   end
+
+  # ajax化
+  def add_menu
+    binding.pry
+    @todaysmenu = current_user.todaysmenus.build(cuisine_id: params[:cuisine_id])
+    @todaysmenu.save
+    @cuisine = Cuisine.find_by(id: params[:cuisine_id])
+  end
+
+  def remove_menu
+    @todaysmenu = current_user.todaysmenus.where(cuisine_id: params[:cuisine_id], created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    binding.pry
+    @todaysmenu.destroy
+    @cuisine = Cuisine.find_by(id: params[:cuisine_id])
+  end
+
+  # ajaxの場合必要?
+  #   当controllerで使用していない
+  private
+  def todaysmenu_params
+    params.require(:todaysmenu).permit(:cuisine_id, :user_id)
+  end
 end
